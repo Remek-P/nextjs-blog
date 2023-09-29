153d29958572ebe26c0eab7aecc4bb2a54654879
Remek-P <remek.piotrowski@wp.pl>
2023-09-29 11:39:09 +0200

    work in progress

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -4,7 +4,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
 
 
 
-module.exports = (phase) =>{
+module.exports = (phase) => {
 
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     const nextConfig = {
